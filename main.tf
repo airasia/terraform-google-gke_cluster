a642710b8d151404521e5861a7bdbbc37cc80861
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-05-01 00:04:33 +0800

    Upgrade version of service_account module

--- main.tf
+++ main.tf
@@ -43,9 +43,9 @@ resource "google_project_service" "networking_api" {
 
 module "gke_service_account" {
   source            = "airasia/service_account/google"
-  version           = "1.0.0"
+  version           = "1.1.0"
   providers         = { google = google }
-  tf_env            = var.name_suffix
+  name_suffix       = var.name_suffix
   account_id        = "gke-sa"
   display_name      = "GKE-ServiceAccount"
   description       = "Its IAM role(s) will specify the access-levels that the GKE node(s) may have"
