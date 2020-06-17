a2d4e3b2605fb222176148e49aaf74adca7091e4
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-06-17 21:02:48 +0800

    Version bump default gke_master_version

--- variables.tf
+++ variables.tf
@@ -62,7 +62,7 @@ variable "sa_name" {
 variable "gke_master_version" {
   description = "GKE version of the cluster master to be used. See https://cloud.google.com/kubernetes-engine/docs/release-notes. "
   type        = string
-  default     = "1.15.11-gke.3"
+  default     = "1.16.9-gke.6"
 }
 
 variable "cluster_description" {
