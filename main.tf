1242cbad330b106e411de713fe858de1969b8a34
sundarsudandiraraj <pecsundar@gmail.com>
2021-05-17 15:53:39 +0800

    Update main.tf
    Co-authored-by: Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -231,7 +231,8 @@ resource "google_compute_address" "static_nginx_ip" {
   }
 }
 
-resource "helm_release" "nginx_controller" {
+resource "helm_release" "nginx_ingress_controller" {
+  # see https://kubernetes.github.io/ingress-nginx/deploy/#using-helm
   # see https://kubernetes.github.io/ingress-nginx/deploy/#using-helm
   count            = var.nginx_controller.enabled ? 1 : 0
   name             = "nginx-ingress"
