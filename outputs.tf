641db9feaf11c4990613c372fbbb63bc7db03ba8
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2021-06-15 23:02:36 +0800

    New output current_master_version

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,8 @@ output "cluster_ca_certificate" {
   description = "Base64 encoded public certificate that is the root of trust for this cluster. Used for connecting to the cluster master via the \"cluster_endpoint\" attribute."
   value       = base64decode(google_container_cluster.k8s_cluster.master_auth.0.cluster_ca_certificate)
 }
+
+output "current_master_version" {
+  description = "Current version number of the GKE cluster master (a.k.a. the control-plane)."
+  value       = google_container_cluster.k8s_cluster.master_version
+}
