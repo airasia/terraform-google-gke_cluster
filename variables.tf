387ca660af455862e234f781f34690b32c43b51a
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2021-10-15 19:41:34 +0800

    New var.default_max_pods_per_node

--- variables.tf
+++ variables.tf
@@ -150,6 +150,12 @@ variable "enable_addon_horizontal_pod_autoscaling" {
   default     = true
 }
 
+variable "default_max_pods_per_node" {
+  description = "The default maximum number of pods per node in this cluster. Every object in the node_pools variable  already has a max_pods_per_node attribute in it. However, this default_max_pods_per_node value is used by the default pool of the cluster when the cluster is being created for the first time - which BTW is deleted by terraform right after creation (see this module's source code for the attribute called 'remove_default_node_pool' which is set to true). So this value is used only for cluster creation and kept small by design. Override this value if you already have a cluster which was created previously with google's default max_pods_per_node value above 8. See https://cloud.google.com/kubernetes-engine/docs/how-to/flexible-pod-cidr"
+  type        = number
+  default     = 8
+}
+
 variable "max_surge" {
   description = "Max number of node(s) that can be over-provisioned while the GKE cluster is undergoing a version upgrade. Raising the number would allow more number of node(s) to be upgraded simultaneously."
   type        = number
