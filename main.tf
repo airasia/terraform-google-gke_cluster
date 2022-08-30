c23ba6e92916583b03eef6e1394ce290198b34b2
Mohamed Fariz Nazeer <faariz@gmail.com>
2022-08-30 20:14:36 +0800

    Add constraint for google provider version as >4.27.0

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 0.13.1" # see https://releases.hashicorp.com/terraform/
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.27.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
+    }
+  }
 }
 
 locals {
